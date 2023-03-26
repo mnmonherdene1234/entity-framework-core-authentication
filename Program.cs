8189b5ee4dd6e7d154de6347c9b1a01ee01c8366
mnmonherdene <mnmonherdene1234@gmail.com>
2023-03-26 04:06:14 +0000

    internet config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using AuthenticationVideoOne.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,10 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkSto
 builder.Services.ConfigureApplicationCookie(config =>
 {
     config.LoginPath = "/Login";
-
+});
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
 });
 
 var app = builder.Build();
@@ -23,11 +28,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseAuthentication();
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
